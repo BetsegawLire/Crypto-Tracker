c8d4f3a3f6f522157328ef065effa19371387dcb
BetsegawLire <betsegawhazard@gmail.com>
2025-10-12 20:00:24 +0300

    Initial Commit

---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by MacBook Pro on 12/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
